50804469f9798c09ccc652c529c059c1b241fd02
Corey Gray <csgray@alaska.edu>
2018-09-11 15:29:39 -0800

    Add exercise 28.

---
+++ ex28.py
@@ -0,0 +1,20 @@
+print(f'01. True and True: {True and True}') # True
+print(f'02. False and True: {False and True}') # False
+print(f'03. 1 == 1 and 2 == 1: {1 == 1 and 2 == 1 }') # False
+print(f'04. "test" == "test": {"test" == "test"}') # True
+print(f'05. 1 == 1 and 2 !=1: {1 == 1 and 2 != 1}') # True
+print(f'06. True and 1 == 1: {True and 1 == 1}') # True
+print(f'07. False and 0 != 0: {False and 0 != 0}') # False
+print(f'08. True or 1 == 1: {True or 1 == 1}') # True
+print(f'09. "test" == "testing": {"test" == "testing"}') # False
+print(f'10. 1 != 0 and 2 == 1: {1 != 0 and 2 == 1}') # False
+print(f'11. "test" != "testing": {"test" != "testing"}') # True
+print(f'12. "test" == 1: {"test" == 1}') # False
+print(f'13. not (True and False): {not (True and False)}') # True
+print(f'14. not (1 == 1 and 0 != 1): {not (1 == 1 and 0 != 1)}') # False
+print(f'15. not (10 == 1 or 1000 == 1000): {not (10 == 1 or 1000 == 1000)}') # False
+print(f'16. not (1 != 10 or 3 == 4): {not (1 != 10 or 3 == 4)}') # False
+print(f'17. not ("testing" == "testing" and "Zed" == "Cool Guy"): {not ("testing" == "testing" and "Zed" == "Cool Guy")}') # True
+print(f'18. 1 == 1 and (not ("testing" == 1 or 1 == 0): {1 == 1 and (not ("testing" == 1 or 1 == 0))}') # True
+print(f'19. "chunky" == "bacon" and (not (3 == 4 or 3 == 3)): {"chunky" == "bacon" and (not (3 == 4 or 3 == 3))}') # False
+print(f'20. 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")): {3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))}') # False
